bf6e10b6ccd4d3a5c44f912727397e2082323c3b
paulocoronado <paulocoronado@gmail.com>
2016-11-01 03:25:00 -0500

    Ocultación del menú en dispositivos xs

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ jQuery(function($) {'use strict';
 
 	$('.navbar-collapse ul li a').on('click', function() {  
 		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		$('.navbar-collapse.in').collapse('hide');
+		
 		return false;
 	});
-
+	
 	// User define function
 	function Scroll() {
 		var contentTop      =   [];
